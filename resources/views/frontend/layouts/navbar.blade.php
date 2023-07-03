a2e4563b5e77b1941cd430d0fcd1a2fd9da3202f
bergforever <mumar1052@gmail.com>
2023-07-03 17:53:42 +0500

    changes in navbar

--- resources/views/frontend/layouts/navbar.blade.php
+++ resources/views/frontend/layouts/navbar.blade.php
@@ -2,7 +2,7 @@
 
     <nav class="bg-white border-gray-200 dark:bg-gray-900">
         <div class="flex flex-wrap items-center justify-between max-w-screen-xl mx-auto p-4">
-            <a href="https://flowbite.com" class="flex items-center">
+            <a href="{{url('/')}}" class="flex items-center">
                 <img src="https://flowbite.com/docs/images/logo.svg" class="h-8 mr-3" alt="Flowbite Logo" />
                 <span class="self-center text-2xl font-semibold whitespace-nowrap text-black">Jmango360</span>
             </a>
